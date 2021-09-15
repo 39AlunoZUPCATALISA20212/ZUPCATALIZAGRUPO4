2c9e0d39cdcf910b14f4186435533a317f7a373e
davi <davi.holanda.userede_ext@zup.com.br>
2021-09-15 18:39:35 -0300

    Adicionei mais algumas models nos moldes do esboço

---
+++ Projeto/src/main/java/br/com/zup/Projeto/Destinatario/Destinatario.java
@@ -0,0 +1,16 @@
+package br.com.zup.Projeto.Destinatario;
+
+import java.time.LocalTime;
+
+public class Destinatario
+{
+    private String nome;
+    private String telefone;
+    private String cpf;
+    private String cep;
+    private LocalTime idade;
+    private EstadoCivil estadoCivil;
+    private int dependentes;
+    private double renda;
+    private OrigemRenda origemRenda;
+}
---
+++ Projeto/src/main/java/br/com/zup/Projeto/Destinatario/EstadoCivil.java
@@ -0,0 +1,12 @@
+package br.com.zup.Projeto.Destinatario;
+
+public enum EstadoCivil
+{
+    MENOR,
+    SOLTEIRA,
+    NAOSOLTEIRA,
+    CORESIDENTE,
+    CASADA,
+    DIVORCIADA,
+    VIUVA
+}
