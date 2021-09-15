73729249a4df5669e4d703db8d75ca392371a3ae
davi <davi.holanda.userede_ext@zup.com.br>
2021-09-15 17:01:11 -0300

    Adicionei a model Doador como especificada no esboço do projeto.

---
+++ Projeto/src/main/java/br/com/zup/Projeto/Doadores/Doador.java
@@ -0,0 +1,20 @@
+package br.com.zup.Projeto.Doadores;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Doadores")
+public class Doador
+{
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String telefone;
+    @Column(nullable = false, unique = true)
+    private String cpf;
+    @Column(nullable = false)
+    private String cep;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+}
