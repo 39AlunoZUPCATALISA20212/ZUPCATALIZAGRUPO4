1be743c74edcfd7aeddf0353b8d84a288ab7a26e
davi <davi.holanda.userede_ext@zup.com.br>
2021-09-25 20:00:46 -0300

    Adicionei as classes feitas pelo Rodolfo, desta vez sem conflito no merge.

---
+++ Projeto/src/main/java/br/com/zup/Projeto/model/UsuarioModel.java
@@ -0,0 +1,19 @@
+package br.com.zup.Projeto.model;
+
+
+import javax.persistence.*;
+
+@Entity(name = "Usuario")
+public class UsuarioModel {
+
+
+    // Estabelcendo a chave primaria
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    // login unico
+    @Column(unique = true)
+    private String login;
+
+    private String password;
+}
